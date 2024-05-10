95dca2244ec31c0758ed96282253408f69dc2caf
Matheus Gonzalez Martinez <matheus.martinez@combocx.com.br>
2024-05-10 15:58:09 -0300

    header

--- js/main.js
+++ js/main.js
@@ -29,12 +29,35 @@ const texts = [
     'Oracle CX Partner Of The Year'
 ];
 
+const buttons = [
+    'entenda mais',
+    'descubra mais',
+    'fale conosco'
+];
+
+const links = [
+    '../varejo.html',
+    '../varejo.html',
+    '../contato.html'
+];
+
 let index = 0;
 
 function changeImage() {
     document.getElementById('header').style.backgroundImage = `url(${images[index]})`;
     document.getElementById('header-text').innerText = texts[index];
+
+    const button = document.getElementById('header-buttons');
+    button.innerText = buttons[index];
+    button.setAttribute('href', links[index]);
+    
     index = (index + 1) % images.length;
+
+    if (buttons[index] === 'entenda mais') {
+        document.getElementById('header-buttons').style.top = '60%';
+    } else {
+        document.getElementById('header-buttons').style.top = '65%';
+    }
 }
 
 setInterval(changeImage, 5000);
