ad29006e287ed953ebe268a057f0195978ce1765
Matheus Gonzalez Martinez <matheus.martinez@combocx.com.br>
2024-05-10 13:24:14 -0300

    add bootstrap e oganização

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-    fetch('header.html')
+    fetch('default/header.html')
         .then(response => response.text())
         .then(data => {
             document.querySelector('header').innerHTML = data;
         });
 
-    fetch('footer.html')
+    fetch('default/footer.html')
         .then(response => response.text())
         .then(data => {
             document.querySelector('footer').innerHTML = data;
