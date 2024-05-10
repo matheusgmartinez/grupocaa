14a34e8dbe5c566d1f198fbc5fac833518ecc9bc
Matheus Gonzalez Martinez <matheus.martinez@combocx.com.br>
2024-05-10 15:03:40 -0300

    header

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(response => response.text())
         .then(data => {
             document.querySelector('header').innerHTML = data;
+
+            const headerText = document.querySelector('#header-text');
+            headerText.innerText = 'Especialistas em negócios e plataformas de tecnologia de CX';
+
+            changeImage();
         });
 
     fetch('default/footer.html')
@@ -11,3 +16,25 @@ document.addEventListener('DOMContentLoaded', function () {
             document.querySelector('footer').innerHTML = data;
         });
 });
+
+const images = [
+    '../images/home-main-banner1.png',
+    '../images/home-main-banner2.png',
+    '../images/home-main-banner3.png'
+];
+
+const texts = [
+    'Especialistas em negócios e plataformas de tecnologia de CX',
+    '10 Most Promising Digital Experience Solutions Provider In Latin America',
+    'Oracle CX Partner Of The Year'
+];
+
+let index = 0;
+
+function changeImage() {
+    document.getElementById('header').style.backgroundImage = `url(${images[index]})`;
+    document.getElementById('header-text').innerText = texts[index];
+    index = (index + 1) % images.length;
+}
+
+setInterval(changeImage, 5000);
